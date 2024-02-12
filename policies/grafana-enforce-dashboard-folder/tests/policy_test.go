070d570e9ab99e206e01d11d1a0ba61e21bd9cb7
Jaksa Vuckovic <vuckovic.jaksa2@zuehlke.com>
2024-02-12 17:12:23 +0000

    first draft of go driven tests

---
+++ policies/grafana-enforce-dashboard-folder/tests/policy_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+	"vap-library/testutils"
+
+	"github.com/lithammer/dedent"
+	"sigs.k8s.io/e2e-framework/pkg/env"
+)
+
+var testenv env.Environment
+
+func TestMain(m *testing.M) {
+	testenv = env.New()
+	// kindClusterName := envconf.RandomName("vap-library-", 16)
+	// testenv.Setup(
+	// 	envfuncs.CreateCluster(kind.NewProvider(), kindClusterName),
+	// )
+
+	// testenv.Finish(
+	// 	envfuncs.DestroyCluster(kindClusterName),
+	// )
+
+	os.Exit(testenv.Run(m))
+}
+
+func TestVapGrafanaEnforceDashboardFolder(t *testing.T) {
+	testutils.CreateFromFile("../policy.yaml", t)
+	testutils.CreateFromFile("binding.yaml", t)
+	testutils.CreateNamespace("my-namespace", t)
+
+	t.Run("dashboard with folder corresponding to namespace should be allowed", func(t *testing.T) {
+		testutils.CreationShouldSucceed(t, dedent.Dedent(`
+			apiVersion: v1
+			kind: ConfigMap
+			metadata:
+			  name: dashboard-in-correct-folder
+			  namespace: my-namespace
+			  labels:
+			    grafana_dashboard: "1"
+			  annotations:
+			    grafana_folder: my-namespace
+			data:
+			  test: "test"`))
+	})
+
+	t.Run("dashboard with folder different from namespace should be denied", func(t *testing.T) {
+		errorMessage := testutils.CreationShouldFail(t, dedent.Dedent(`
+			apiVersion: v1
+			kind: ConfigMap
+			metadata:
+			  name: dashboard-in-wrong-folder
+			  namespace: my-namespace
+			  labels:
+			    grafana_dashboard: "1"
+			  annotations:
+			    grafana_folder: "some-other-folder"
+			data:
+			  test: "test"`))
+
+		if !strings.HasSuffix(errorMessage, "exceeds the maximum of 4Gi") {
+			t.Errorf("Unexpected error message: %s", errorMessage)
+		}
+	})
+}
---
+++ testutils/testutils.go
@@ -0,0 +1,102 @@
+package testutils
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/e2e-framework/klient/k8s"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+)
+
+func CreateVapFromFile3(filename string, ctx context.Context, c *envconf.Config) (k8s.Object, error) {
+	fmt.Println("Creating VAP from file", filename)
+
+	content, _ := os.ReadFile(filename)
+	object, _, err := scheme.Codecs.UniversalDeserializer().Decode(content, nil, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode VAP: %v", err)
+	}
+
+	client, err := c.NewClient()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create client: %v", err)
+	}
+
+	o, _ := object.(k8s.Object)
+
+	err = client.Resources().Create(ctx, o)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create VAP: %v", err)
+	}
+
+	return o, nil
+}
+
+func CreateFromFile(filename string, t *testing.T) {
+	out, err := runCommand("kubectl", "apply", "-f", filename)
+	if err != nil {
+		t.Fatalf("failed to create kubernetes resources: %v", out)
+	}
+	fmt.Println(out)
+}
+
+func CreateNamespace(name string, t *testing.T) {
+	out, err := runCommand("kubectl", "create", "namespace", name)
+	if err != nil {
+		t.Fatalf("failed to create namespace: %v", out)
+	}
+	fmt.Println(out)
+}
+
+func Destroy(id string) {
+	fmt.Println("Destroying", id)
+}
+
+func CreationShouldSucceed(t *testing.T, resourceDef string) {
+	out, err := runCommandWithInput(resourceDef, "kubectl", "apply", "-f", "-")
+	if err != nil {
+		t.Fatalf("Creation failed: %v", out)
+	}
+}
+
+func CreationShouldFail(t *testing.T, resourceDef string) string {
+	out, err := runCommandWithInput(resourceDef, "kubectl", "apply", "-f", "-")
+	if err == nil {
+		t.Fatalf("Creation should have failed, but it succeeded: %v", out)
+	}
+	return out
+}
+
+func runCommandWithInput(input string, name string, args ...string) (string, error) {
+	cmd := exec.Command(name, args...)
+	cmd.Stdin = bytes.NewBufferString(input)
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+	cmd.Stdout = buf
+	cmd.Stderr = errBuf
+	err := cmd.Run()
+	if err != nil {
+		return errBuf.String(), err
+	}
+
+	return buf.String(), nil
+}
+
+func runCommand(name string, args ...string) (string, error) {
+	cmd := exec.Command(name, args...)
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+	cmd.Stdout = buf
+	cmd.Stderr = errBuf
+	err := cmd.Run()
+	if err != nil {
+		return errBuf.String(), err
+	}
+
+	return buf.String(), nil
+}
