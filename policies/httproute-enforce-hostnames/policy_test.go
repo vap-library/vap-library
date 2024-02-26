b67fdfb2fa64848fc77ed3e273c49a1daad6f7ed
Balazs Zachar <balazs.zachar@ctbto.org>
2024-02-26 19:27:11 +0100

    remove some debug text and remove applying the CRD twice

--- policies/httproute-enforce-hostnames/policy_test.go
+++ policies/httproute-enforce-hostnames/policy_test.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"sigs.k8s.io/e2e-framework/klient/decoder"
-	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 	"testing"
@@ -20,11 +18,8 @@ func TestMain(m *testing.M) {
 	var err error
 	testEnv, err = testutils.CreateTestEnv("")
 	if err != nil {
-		fmt.Println("haho")
 		fmt.Print(err)
 		os.Exit(1)
-	} else {
-		fmt.Println("minden ok")
 	}
 
 	os.Exit(testEnv.Run(m))
@@ -33,14 +28,14 @@ func TestMain(m *testing.M) {
 func TestKindCluster(t *testing.T) {
 	feature := features.New("Testing applying resources").
 		Setup(func(ctx context.Context, t *testing.T, config *envconf.Config) context.Context {
-			r, err := resources.New(config.Client().RESTConfig())
-			if err != nil {
-				t.Fatal(err)
-			}
-			err = decoder.ApplyWithManifestDir(ctx, r, "./", "*.yaml", []resources.CreateOption{})
-			if err != nil {
-				t.Fatal(err)
-			}
+			//r, err := resources.New(config.Client().RESTConfig())
+			//if err != nil {
+			//	t.Fatal(err)
+			//}
+			//err = decoder.ApplyWithManifestDir(ctx, r, "./", "*.yaml", []resources.CreateOption{})
+			//if err != nil {
+			//	t.Fatal(err)
+			//}
 			return ctx
 		}).Assess("Nginx pod can call github api", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
 		return ctx
