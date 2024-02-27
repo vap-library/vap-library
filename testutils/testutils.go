1847621bd5845c902e255f30ec66e8cc75536c14
Balazs Zachar <balazs.zachar@ctbto.org>
2024-02-27 17:37:52 +0100

    create namespace with labels and apply all yaml files from the policy directory

--- testutils/testutils.go
+++ testutils/testutils.go
@@ -2,19 +2,26 @@ package testutils
 
 import (
 	"context"
-	"fmt"
+	"sigs.k8s.io/e2e-framework/klient/decoder"
+	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
 	"sigs.k8s.io/e2e-framework/pkg/env"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/envfuncs"
 	"sigs.k8s.io/e2e-framework/support/kind"
-	"sigs.k8s.io/e2e-framework/support/utils"
+	"time"
 )
 
-func CreateTestEnv(kindVersion string) (env.Environment, error) {
+const (
+	defaultKindVersion = "v1.29.2"
+	kindNamePrefix     = "vaplibtest"
+	testNamespace      = "vap-testing"
+)
+
+func CreateTestEnv(kindVersion string, keepLogs bool, namespaceLabels map[string]string) (env.Environment, error) {
 
 	// Hard code a kind version as default
 	if kindVersion == "" {
-		kindVersion = "v1.29.2"
+		kindVersion = defaultKindVersion
 	}
 
 	var testEnv env.Environment
@@ -25,47 +32,52 @@ func CreateTestEnv(kindVersion string) (env.Environment, error) {
 	var finishFuncs []env.Func
 
 	// Create cluster
-	kindClusterName := envconf.RandomName("vaplibtest-", 16)
+	kindClusterName := envconf.RandomName(kindNamePrefix, 16)
 	setupFuncs = append(setupFuncs, envfuncs.CreateClusterWithConfig(kind.NewProvider(), kindClusterName, "../../testutils/kind-config.yaml", kind.WithImage("kindest/node:"+kindVersion)))
 
-	// Apply the CRD for the parameter if we got one
-	crdFileName := "./crd-parameter.yaml"
+	// Apply all yaml from the policy directory
 	setupFuncs = append(
 		setupFuncs,
 		func(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
-			if p := utils.RunCommand(fmt.Sprintf("kubectl apply --server-side -f %s", crdFileName)); p.Err() != nil {
-				return ctx, p.Err()
+			r, err := resources.New(cfg.Client().RESTConfig())
+			if err != nil {
+				return ctx, err
 			}
+			err = decoder.ApplyWithManifestDir(ctx, r, "./", "*.yaml", []resources.CreateOption{})
+			if err != nil {
+				return ctx, err
+			}
+
+			// Sleep 2 sec to make sure the API has the VAP and binding properly "registered"
+			time.Sleep(2 * time.Second)
+
 			return ctx, nil
 		},
 	)
 
-	//if _, err := os.Stat(crdFileName); err == nil {
-	//	setupFuncs = append(
-	//		setupFuncs,
-	//		func(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
-	//			// Deploy the controller components
-	//			if p := utils.RunCommand(fmt.Sprintf("kubectl apply --server-side -f %s", crdFileName)); p.Err() != nil {
-	//				return ctx, p.Err()
-	//			}
-	//			return ctx, nil
-	//		},
-	//	)
-	//
-	//	setupFuncs = append(
-	//		finishFuncs,
-	//		func(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
-	//			utils.RunCommand(fmt.Sprintf("kubectl delete -f %s", crdFileName))
-	//			return ctx, nil
-	//		},
-	//	)
-	//} else {
-	//	return nil, err
-	//}
+	// Create a namespace for the test with the labels
+	setupFuncs = append(
+		setupFuncs,
+		envfuncs.CreateNamespace(testNamespace, envfuncs.WithLabels(namespaceLabels)),
+	)
+
+	// Apply the CRD for the parameter if we got one
+	//crdFileName := "./crd-parameter.yaml"
+	//setupFuncs = append(
+	//	setupFuncs,
+	//	func(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
+	//		if p := utils.RunCommand(fmt.Sprintf("kubectl apply --server-side -f %s", crdFileName)); p.Err() != nil {
+	//			return ctx, p.Err()
+	//		}
+	//		return ctx, nil
+	//	},
+	//)
 
 	testEnv.Setup(setupFuncs...)
 
-	finishFuncs = append(finishFuncs, envfuncs.ExportClusterLogs(kindClusterName, "./test-logs"))
+	if keepLogs {
+		finishFuncs = append(finishFuncs, envfuncs.ExportClusterLogs(kindClusterName, "./test-logs"))
+	}
 	finishFuncs = append(finishFuncs, envfuncs.DestroyCluster(kindClusterName))
 
 	testEnv.Finish(finishFuncs...)
