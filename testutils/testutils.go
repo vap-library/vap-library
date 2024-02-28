5790be4084b6df1f3a7edf3774a629e26be3b611
Balazs Zachar <balazs.zachar@ctbto.org>
2024-02-28 13:42:52 +0100

    switch to use one namespace per test to make sure they do not collide

--- testutils/testutils.go
+++ testutils/testutils.go
@@ -2,12 +2,16 @@ package testutils
 
 import (
 	"context"
+	"fmt"
+	v1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/e2e-framework/klient/decoder"
 	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
 	"sigs.k8s.io/e2e-framework/pkg/env"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/envfuncs"
 	"sigs.k8s.io/e2e-framework/support/kind"
+	"strings"
+	"testing"
 	"time"
 )
 
@@ -17,7 +21,15 @@ const (
 	testNamespace      = "vap-testing"
 )
 
+type (
+	NamespaceCtxKey string
+	ClusterCtxKey   string
+)
+
 func CreateTestEnv(kindVersion string, keepLogs bool, namespaceLabels map[string]string) (env.Environment, error) {
+
+	// Specifying a run ID so that multiple runs wouldn't collide.
+	runID := envconf.RandomName(testNamespace, 14)
 
 	// Use the default Kind version if none is provided
 	if kindVersion == "" {
@@ -56,12 +68,6 @@ func CreateTestEnv(kindVersion string, keepLogs bool, namespaceLabels map[string
 		},
 	)
 
-	// Create a namespace for the test with the labels
-	setupFuncs = append(
-		setupFuncs,
-		envfuncs.CreateNamespace(testNamespace, envfuncs.WithLabels(namespaceLabels)),
-	)
-
 	// Apply the CRD for the parameter if we got one
 	//crdFileName := "./crd-parameter.yaml"
 	//setupFuncs = append(
@@ -76,8 +82,21 @@ func CreateTestEnv(kindVersion string, keepLogs bool, namespaceLabels map[string
 
 	testEnv.Setup(setupFuncs...)
 
-	// Remove the test namespace
-	finishFuncs = append(finishFuncs, envfuncs.DeleteNamespace(testNamespace))
+	// Remove the applied resources
+	finishFuncs = append(
+		finishFuncs,
+		func(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
+			r, err := resources.New(cfg.Client().RESTConfig())
+			if err != nil {
+				return ctx, err
+			}
+			err = decoder.DeleteWithManifestDir(ctx, r, "./", "*.yaml", []resources.DeleteOption{})
+			if err != nil {
+				return ctx, err
+			}
+			return ctx, nil
+		},
+	)
 
 	// Keep the logs if the flag is set
 	if keepLogs {
@@ -89,5 +108,47 @@ func CreateTestEnv(kindVersion string, keepLogs bool, namespaceLabels map[string
 
 	testEnv.Finish(finishFuncs...)
 
+	// Set the BeforeEachTest and AfterEachTest functions that creates and deletes a namespace for each test
+	testEnv.BeforeEachTest(func(ctx context.Context, cfg *envconf.Config, t *testing.T) (context.Context, error) {
+		return createNSForTest(ctx, cfg, t, runID, namespaceLabels)
+	})
+	testEnv.AfterEachTest(func(ctx context.Context, cfg *envconf.Config, t *testing.T) (context.Context, error) {
+		return deleteNSForTest(ctx, cfg, t, runID)
+	})
+
 	return testEnv, nil
+}
+
+// createNSForTest creates a random namespace with the runID as a prefix. It is stored in the context
+// so that the deleteNSForTest routine can look it up and delete it.
+func createNSForTest(ctx context.Context, cfg *envconf.Config, t *testing.T, runID string, namespaceLabels map[string]string) (context.Context, error) {
+	ns := envconf.RandomName(runID, 20)
+	ctx = context.WithValue(ctx, GetNamespaceKey(t), ns)
+
+	t.Logf("Creating NS %v for test %v", ns, t.Name())
+	nsObj := v1.Namespace{}
+	nsObj.Name = ns
+	nsObj.Labels = namespaceLabels
+	return ctx, cfg.Client().Resources().Create(ctx, &nsObj)
+}
+
+// deleteNSForTest looks up the namespace corresponding to the given test and deletes it.
+func deleteNSForTest(ctx context.Context, cfg *envconf.Config, t *testing.T, _ string) (context.Context, error) {
+	ns := fmt.Sprint(ctx.Value(GetNamespaceKey(t)))
+	t.Logf("Deleting NS %v for test %v", ns, t.Name())
+
+	nsObj := v1.Namespace{}
+	nsObj.Name = ns
+	return ctx, cfg.Client().Resources().Delete(ctx, &nsObj)
+}
+
+// GetNamespaceKey returns the context key for a given test
+func GetNamespaceKey(t *testing.T) NamespaceCtxKey {
+	// When we pass t.Name() from inside an `assess` step, the name is in the form TestName/Features/Assess
+	if strings.Contains(t.Name(), "/") {
+		return NamespaceCtxKey(strings.Split(t.Name(), "/")[0])
+	}
+
+	// When pass t.Name() from inside a `testenv.BeforeEachTest` function, the name is just TestName
+	return NamespaceCtxKey(t.Name())
 }
