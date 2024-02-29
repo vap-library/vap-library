5d681abdcaf5aa02fb24ef0cad17e481e7a1e470
Balazs Zachar <balazs.zachar@ctbto.org>
2024-02-29 14:45:29 +0100

    implement tests for the grafana dashboard policy

--- policies/grafana-enforce-dashboard-folder/policy_test.go
+++ policies/grafana-enforce-dashboard-folder/policy_test.go
@@ -3,16 +3,54 @@ package httproute_enforce_hostnames
 import (
 	"context"
 	"fmt"
-	v1 "k8s.io/api/core/v1"
 	"log"
 	"os"
+	"sigs.k8s.io/e2e-framework/klient/decoder"
+	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
+	"strings"
 	"testing"
 	"vap-library/testutils"
 
 	"sigs.k8s.io/e2e-framework/pkg/env"
 )
+
+var dashboardCMYAML string = `
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: test-dashboard
+  namespace: %s
+  labels:
+    grafana_dashboard: "1"
+  annotations:
+    grafana_folder: %s
+data:
+  test: "test"
+`
+
+var dashboardCMWithoutAnnotationYAML string = `
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: test-dashboard
+  namespace: %s
+  labels:
+    grafana_dashboard: "1"
+data:
+  test: "test"
+`
+
+var normalCMYAML string = `
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: test-dashboard
+  namespace: %s
+data:
+  test: "test"
+`
 
 var testEnv env.Environment
 
@@ -28,37 +66,98 @@ func TestMain(m *testing.M) {
 	os.Exit(testEnv.Run(m))
 }
 
-func TestVAPGrafanaEnforceDashboardFolder(t *testing.T) {
+func TestVAPGrafanaEnforceDashboardFolderValid(t *testing.T) {
 
-	f := features.New("pod list").
-		Assess("pods from namespace", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			var pods v1.PodList
-			namespace := ctx.Value(testutils.GetNamespaceKey(t)).(string)
-			err := cfg.Client().Resources(namespace).List(context.TODO(), &pods)
+	f := features.New("Dashboard is accepted").
+		Assess("A valid dashboard ConfigMap is accepted", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			r, err := resources.New(cfg.Client().RESTConfig())
 			if err != nil {
 				t.Fatal(err)
 			}
-			t.Logf("found %d pods in namespace %s", len(pods.Items), namespace)
+
+			// get namespace
+			namespace := ctx.Value(testutils.GetNamespaceKey(t)).(string)
+
+			// decode CM yaml
+			cmObj, err := decoder.DecodeAny(strings.NewReader(fmt.Sprintf(dashboardCMYAML, namespace, namespace)))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// apply CM
+			handler := decoder.CreateHandler(r)
+			t.Logf("applying valid dashboard CM to namespace %s", namespace)
+			if err := handler(ctx, cmObj); err != nil {
+				t.Fatal(err)
+			}
+
 			return ctx
 		})
 
 	_ = testEnv.Test(t, f.Feature())
 
-	// Create a feature
-	//feature := features.New("Testing applying resources").
-	//	Setup(func(ctx context.Context, t *testing.T, config *envconf.Config) context.Context {
-	//		//r, err := resources.New(config.Client().RESTConfig())
-	//		//if err != nil {
-	//		//	t.Fatal(err)
-	//		//}
-	//		//err = decoder.ApplyWithManifestDir(ctx, r, "./", "*.yaml", []resources.CreateOption{})
-	//		//if err != nil {
-	//		//	t.Fatal(err)
-	//		//}
-	//		return ctx
-	//	}).Assess("Nginx pod can call github api", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
-	//	return ctx
-	//}).Feature()
+}
 
-	//_ = testEnv.Test(t, feature)
+func TestVAPGrafanaEnforceDashboardFolderNormalCM(t *testing.T) {
+
+	f := features.New("Normal CM is accepted").
+		Assess("A non-dashboard CM is accepted", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			r, err := resources.New(cfg.Client().RESTConfig())
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// get namespace
+			namespace := ctx.Value(testutils.GetNamespaceKey(t)).(string)
+
+			// decode CM yaml
+			cmObj, err := decoder.DecodeAny(strings.NewReader(fmt.Sprintf(normalCMYAML, namespace)))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// apply CM
+			handler := decoder.CreateHandler(r)
+			t.Logf("applying a normal ConfigMap to namespace %s", namespace)
+			if err := handler(ctx, cmObj); err != nil {
+				t.Fatal(err)
+			}
+
+			return ctx
+		})
+
+	_ = testEnv.Test(t, f.Feature())
+
+}
+
+func TestVAPGrafanaEnforceDashboardFolderInvalidDashboardCM(t *testing.T) {
+
+	f := features.New("Normal CM is accepted").
+		Assess("A non-dashboard CM is accepted", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			r, err := resources.New(cfg.Client().RESTConfig())
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// get namespace
+			namespace := ctx.Value(testutils.GetNamespaceKey(t)).(string)
+
+			// decode CM yaml
+			cmObj, err := decoder.DecodeAny(strings.NewReader(fmt.Sprintf(dashboardCMWithoutAnnotationYAML, namespace)))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// apply CM
+			handler := decoder.CreateHandler(r)
+			t.Logf("trying to apply an invalid ConfigMap to namespace %s", namespace)
+			if err := handler(ctx, cmObj); err == nil {
+				t.Fatal("expected error, but it was possible to apply an invalid dashboard ConfigMap")
+			}
+
+			return ctx
+		})
+
+	_ = testEnv.Test(t, f.Feature())
+
 }
