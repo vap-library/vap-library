70225d4399efc7aa97ced9a8d7fac85597d70c27
James King <james.king@mobilise.cloud>
2024-06-26 09:57:34 +0100

    Ensure ParamRef is added to required bindings in generated release output Also correct policy namespace labels to be lowercase. Also add generation of kustomization file Also add section in README to describe use of new release process

--- release-process/release/release.py
+++ release-process/release/release.py
@@ -7,11 +7,12 @@ import os
 import sys
 
 class Release:
-    def __init__(self, config_path, policies_domain, policies_output_filename, bindings_output_filename, crds_output_filename):
+    def __init__(self, config_path, policies_domain, policies_output_filename, bindings_output_filename, crds_output_filename, kustomization_output_filename):
         self.__policies_domain = policies_domain
         self.__policies_file = policies_output_filename
         self.__bindings_file = bindings_output_filename
         self.__crds_file = crds_output_filename
+        self.__kustomization_file = kustomization_output_filename
         self.__config = self.__parse_config(config_path)
 
     def __parse_config(self, config_path):
@@ -20,7 +21,7 @@ class Release:
         return config
     
     def __append_policy(self, subdirectory):
-        policy_path = os.path.join('policies', subdirectory, 'policy.yaml')
+        policy_path = os.path.join('../../policies', subdirectory, 'policy.yaml')
         if os.path.exists(policy_path):
             with open(policy_path, 'r') as file:
                 policy_content = file.read().rstrip('\n').lstrip('---\n')
@@ -31,8 +32,8 @@ class Release:
         else:
           print(f"Warning: {policy_path} does not exist.")
 
-    def __create_binding(self, policy_name, binding_name, actions):
-        return {
+    def __create_binding(self, policy_name, binding_name, actions, param_ref=None):
+        binding = {
             'apiVersion': 'admissionregistration.k8s.io/v1beta1',
             'kind': 'ValidatingAdmissionPolicyBinding',
             'metadata': {
@@ -42,27 +43,34 @@ class Release:
                 'matchResources': {
                     'matchPolicy': 'Equivalent',
                     'namespaceSelector': {
-                        'matchLabels': {f'vap-library.com/{policy_name}': actions[0]}
+                        'matchLabels': {f'vap-library.com/{policy_name}': actions[0].lower()}
                     },
-                'objectSelector': {}
-            },
-            'policyName': f'{policy_name}.{self.__policies_domain}',
-            'validationActions': actions
+                    'objectSelector': {}
+                },
+                'policyName': f'{policy_name}.{self.__policies_domain}',
+                'validationActions': actions
             }
         }
+        if param_ref is not None:
+            binding['spec']['paramRef'] = param_ref
+        return binding
     
     def __append_bindings(self, policy_name, bindings):
         for binding in bindings:
           for key in binding:
             actions = binding[key]['validationActions']
-            binding_object = self.__create_binding(policy_name, key, actions)
-        
+            if 'paramRef' in binding[key]:
+                param_ref = binding[key]['paramRef']
+                binding_object = self.__create_binding(policy_name, key, actions, param_ref)
+            else:
+                binding_object = self.__create_binding(policy_name, key, actions)
+
           with open(self.__bindings_file, 'a') as file:
-              yaml.dump([binding_object], file, default_flow_style=False)
+              yaml.dump(binding_object, file, default_flow_style=False)
               file.write('---\n')
     
     def __append_crds(self, subdirectory):
-        crd_path = os.path.join('policies', subdirectory, 'crd-parameter.yaml')
+        crd_path = os.path.join('../../policies', subdirectory, 'crd-parameter.yaml')
         if os.path.exists(crd_path):
             with open(crd_path, 'r') as file:
                 policy_content = file.read().rstrip('\n').lstrip('---\n')
@@ -73,6 +81,17 @@ class Release:
         else:
           print(f"Warning: {crd_path} does not exist.")
 
+    def __append_kustomization(self, subdirectory):
+        kustomization_config = {
+            'apiVersion': 'kustomize.config.k8s.io/v1beta1',
+            'kind': 'Kustomization',
+            'resources': [self.__bindings_file, self.__crds_file, self.__policies_file]
+        }
+
+        with open(self.__kustomization_file, 'a') as file:
+              yaml.dump(kustomization_config, file, default_flow_style=False)
+
+
     def generate_release_files(self):
         # Ensure the output files are empty at the start
         with open(self.__policies_file, 'w') as file:
@@ -82,6 +101,9 @@ class Release:
             pass
     
         with open(self.__crds_file, 'w') as file:
+            pass
+        
+        with open(self.__kustomization_file, 'w') as file:
             pass
     
         for subdirectory, details in self.__config.items():
@@ -95,20 +117,25 @@ class Release:
                     self.__append_bindings(subdirectory, bindings)
                 # Add CRDs for the policy if they exist
                 self.__append_crds(subdirectory)
+        
+        # Generate kustomization config
+        self.__append_kustomization(subdirectory)
 
 
 def main(config_path):
-    policies_file = 'release-policies.yaml'
-    bindings_file = 'release-bindings.yaml'
-    crds_file = 'release-crds.yaml'
+    policies_file = 'policies.yaml'
+    bindings_file = 'bindings.yaml'
+    crds_file = 'crds.yaml'
+    kustomization_file = 'kustomization.yaml'
     policies_domain = 'vap-library.com'
 
-    new_release = Release(config_path, policies_domain, policies_file, bindings_file, crds_file)
+    new_release = Release(config_path, policies_domain, policies_file, bindings_file, crds_file, kustomization_file)
     new_release.generate_release_files()
         
     print(f"Policies have been appended to {policies_file}")
     print(f"Bindings have been appended to {bindings_file}")
     print(f"CRDs have been appended to {crds_file}, if they exist")
+    print(f"Kustomization generated at {kustomization_file}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
